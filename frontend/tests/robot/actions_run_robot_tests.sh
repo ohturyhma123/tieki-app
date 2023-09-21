af62a9e0b4e3f9a6e570c997038aea25594f8403
katajak <101673344+katajak@users.noreply.github.com>
2023-09-21 15:04:29 +0300

    fix status code

--- frontend/tests/robot/actions_run_robot_tests.sh
+++ frontend/tests/robot/actions_run_robot_tests.sh
@@ -6,7 +6,9 @@ npm run dev &
 # Run robot tests headless
 robot --variable browser:headlesschrome --variable delay:0 tests/robot/
 
+status=$?
+
 # Stop server
 killall -s SIGINT node
 
-exit
+exit $status
