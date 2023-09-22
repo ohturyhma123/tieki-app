52d6b5b1af94bfc447b10a1bc6ac059050b5f932
Nikita Essine <54572118+nikitaessine@users.noreply.github.com>
2023-09-22 14:40:12 +0300

    Update actions_run_robot_tests.sh

--- tests/robot/actions_run_robot_tests.sh
+++ tests/robot/actions_run_robot_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start server in background
-npm run dev &
+cd frontend && npm run dev &
 
 # Run robot tests headless
 robot --variable browser:headlesschrome --variable delay:0 tests/robot/
