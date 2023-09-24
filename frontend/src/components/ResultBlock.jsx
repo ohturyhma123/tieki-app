52a17674927884a6eeef238d0f661884c0505ef1
w <weverhall@gmail.com>
2023-09-24 17:21:25 +0300

    run lint-fix w/ updated rules

--- frontend/src/components/ResultBlock.jsx
+++ frontend/src/components/ResultBlock.jsx
@@ -1,42 +1,42 @@
-import { useState } from "react"
+import { useState } from 'react'
 import '../assets/Statement.css'
 import '../assets/Link.css'
 
-const ResultBlock = ({result}) => {
+const ResultBlock = ({ result }) => {
 
-    const [toggle, setToggle] = useState(false)
-    const handleToggle = () => setToggle(!toggle)
+  const [toggle, setToggle] = useState(false)
+  const handleToggle = () => setToggle(!toggle)
 
-    return(
-        <>
-            <h3>{result.category}</h3>
-            <p onClick={handleToggle} className="statement">{toggle ? '> piilota analyysi' : '> nää analyysi'}</p>
-            <div key={result.id} className={toggle ? "statement" : ""}>
-                {toggle
-                  ? result.textSegments.map((result) => <p key={result.id}>{result}</p>)
-                  : null
-                }
-                <ul>
-                  {result.listPoints && toggle
-                    ? result.listPoints.map((item, index) => <li key={index}>{item}</li>)
-                    : null
-                  }
-                </ul>
-                <p>Hyödyllisiä linkkejä aiheista:</p>
-                {result.links && toggle
-                  ? result.links.map((link) => {
-                    return(
-                      <>
-                        <a className="link" href={link}>{link}</a>
-                        <br></br>
-                      </>
-                    )
-                  })
-                  : null
-                }
-            </div>
-        </>
-    )
+  return(
+    <>
+      <h3>{result.category}</h3>
+      <p onClick={handleToggle} className="statement">{toggle ? '> piilota analyysi' : '> nää analyysi'}</p>
+      <div key={result.id} className={toggle ? 'statement' : ''}>
+        {toggle
+          ? result.textSegments.map((result) => <p key={result.id}>{result}</p>)
+          : null
+        }
+        <ul>
+          {result.listPoints && toggle
+            ? result.listPoints.map((item, index) => <li key={index}>{item}</li>)
+            : null
+          }
+        </ul>
+        <p>Hyödyllisiä linkkejä aiheista:</p>
+        {result.links && toggle
+          ? result.links.map((link) => {
+            return(
+              <>
+                <a className="link" href={link}>{link}</a>
+                <br></br>
+              </>
+            )
+          })
+          : null
+        }
+      </div>
+    </>
+  )
 }
 
 export default ResultBlock
