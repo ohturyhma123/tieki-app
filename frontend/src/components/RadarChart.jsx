6f5f3f17936d9d60956ead8b3b09a8e2586d2e0e
katajak <101673344+katajak@users.noreply.github.com>
2023-10-01 20:40:41 +0300

    unittesting with jest

--- frontend/src/components/RadarChart.jsx
+++ frontend/src/components/RadarChart.jsx
@@ -8,6 +8,7 @@ import {
   Legend,
 } from 'chart.js'
 import { Radar } from 'react-chartjs-2'
+import splitLongStrings from './splitLongStrings'
 import '../assets/RadarChart.css'
 
 ChartJS.register(
@@ -20,32 +21,6 @@ ChartJS.register(
 )
 
 const RadarChart = ({ categories, results }) => {
-  const splitLongStrings = (arr, maxLength) => {
-    return arr.map((str) => {
-      if (str.length <= maxLength) {
-        return [str] //**If the string is shorter than or equal to maxLength, no splitting needed */
-      }
-      const chunks = []
-      let start = 0
-      while (start < str.length) {
-        let end = start + maxLength
-
-        //**Adjust the end index to the last space within the maxLength */
-        while (end > start && str[end] !== ' ' && str[end] !== undefined) {
-          end--
-        }
-
-        const chunk = str.substring(start, end).trim()
-        if (chunk.length > 0) {
-          chunks.push(chunk)
-        }
-        start = end + 1 //** Move start to the next character after the space */
-      }
-
-      return chunks
-    })
-  }
-
   const data = {
     labels: splitLongStrings(categories, 20),
     datasets: [
