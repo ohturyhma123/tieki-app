0a49643fa5865f5c09370ce0356f6b84e05f9382
alanenpa <72764484+alanenpa@users.noreply.github.com>
2023-10-07 16:41:27 +0300

    Add MUI elements to frontpage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { List, ListItem, Typography, Paper, Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
@@ -5,22 +6,34 @@ const Home = () => {
   const statementsRoute = () => { navigate('/test/0') }
 
   return (
-    <div>
-      <h1>Tieteellisen kirjoittamisen itsearviointitesti</h1>
-      <p>
+    <Paper sx={{ m: 5, p: 10 }} variant='elevation'>
+      <Typography id='frontpageHeader' variant='h4'>Tieteellisen kirjoittamisen itsearviointitesti</Typography>
+      <Typography sx={{ pt: 2 }} variant='subtitle1'>
         Tämän testin avulla voit arvioida osaamistasi tieteellisen tekstin kirjoittajana.<br />
         Testissä on väitteitä seuraavilta kirjoittamisen osa-alueilta:
-      </p>
-      <ul>
-        <li>Kirjoittamiseen liittyvät tunteet ja ajatukset</li>
-        <li>Akateemiset käytänteet ja tekstilajit</li>
-        <li>Lukeminen ja lähteiden käyttö</li>
-        <li>Kirjoittaminen prosessina</li>
-        <li>Tekstin rakenne</li>
-        <li>Kielelliset seikat</li>
-      </ul>
-      <button id='statementsRoute' onClick={statementsRoute}>Testiin!</button>
-    </div>
+      </Typography>
+      <List sx={{ listStyleType: 'disc', pl: 4, mb: 2 }}>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Kirjoittamiseen liittyvät tunteet ja ajatukset</Typography>
+        </ListItem>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Akateemiset käytänteet ja tekstilajit</Typography>
+        </ListItem>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Lukeminen ja lähteiden käyttö</Typography>
+        </ListItem>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Kirjoittaminen prosessina</Typography>
+        </ListItem>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Tekstin rakenne</Typography>
+        </ListItem>
+        <ListItem dense sx={{ display: 'list-item' }}>
+          <Typography>Kielelliset seikat</Typography>
+        </ListItem>
+      </List>
+      <Button sx={{ mr: 1 }} variant="contained" id='statementsRoute' onClick={statementsRoute}>Testiin!</Button>
+    </Paper>
   )
 }
 
