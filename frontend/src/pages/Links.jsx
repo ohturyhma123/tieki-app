4c6269ba517ed913e4aab6aa287bce632c61aa6a
alanenpa <72764484+alanenpa@users.noreply.github.com>
2023-10-08 19:27:40 +0300

    Add MUI elements to link page

--- frontend/src/pages/Links.jsx
+++ frontend/src/pages/Links.jsx
@@ -1,31 +1,90 @@
-const Links = () => {
-  return (
-    <div>
-      <h1>Hyödyllisiä linkkejä</h1>
-      <p>
-      Lue lisää yhdessä kirjoittamisesta: <a href='https://webcgi.oulu.fi/oykk/abc/kirjoittamisprosessi/yhdessa_kirjoittaminen_ja_yhteiskirjoittaminen/' target="_blank" rel="noopener noreferrer"> (Kirjoittajan ABC)</a> <br />
-       Lue lisää motivaatiosta ja tavoitteiden asettamisesta: <a href='https://gradutakuu.fi/sisallys/' target="_blank" rel="noopener noreferrer"> (Gradutakuu)</a>  <br />
-       Lue lisää akateemisista tekstitaidoista: <a href ='https://kokonaisvaltainenkirjoittaminen.fi/2011/02/15/akateemisen-kirjoittamisen-opetus/' target="_blank" rel="noopener noreferrer">(Kokonaisvaltainen kirjoittaminen -blogi)</a>  <br />
-       Lue tieteellisen referoimisesta (luku 6) e-kirjasta <a href='https://www.ellibslibrary.com/book/9789523455115' target="_blank" rel="noopener noreferrer">Tiede ja teksti - Tehoa ja taitoa tutkielman kirjoittamiseen</a> (lainattavissa Haka-tunnuksilla ja kirjastoista) <br />
-       Lue lisää palautteesta: <a href = 'http://www.plotti.fi/2020/08/14/palautetta-hopeatarjottimella-vaikuta-palautekokemuksiisi/' target="_blank" rel="noopener noreferrer">(Plotti)</a> <br />
-       Lue kirjoittamisen sosiaalisesta luonteesta: <a href='https://antroblogi.fi/2017/06/sinun-tekstisi-ei-ole-sinun/' target="_blank" rel="noopener noreferrer">(Sinun tekstisi ei ole sinun)</a> <br />
-       Lue tieteellisen tekstin rakenteesta ja argumentoinnista (luvut 4 ja 5) e-kirjasta <a href='https://www.ellibslibrary.com/book/9789523455115' target="_blank" rel="noopener noreferrer">Tiede ja teksti - Tehoa ja taitoa tutkielman kirjoittamiseen</a> (lainattavissa Haka-tunnuksilla ja kirjastoista) <br />
-        <a href='http://www.kielitoimistonohjepankki.fi/' target="_blank" rel="noopener noreferrer">Kielitoimiston ohjepankki </a> <br />
-       Tukea tekstin viimeistelyyn: <a href='https://blogs.helsinki.fi/kielijelppi/viimeistely/' target="_blank" rel="noopener noreferrer">(Kielijelppi)</a> <br />
-       Kangertaako kandityö?: <a href='http://www.plotti.fi/2018/01/29/kangertaako-kandityo/' target="_blank" rel="noopener noreferrer">(Plotti)</a> <br />
-       Miten vähentää prokrastinaatiota?: <a href='https://www.nyyti.fi/opiskelijoille/opi-elamantaitoa/ajanhallinta/prokrastinaatio/miten-vahentaa-prokrastinaatiota/' target="_blank" rel="noopener noreferrer">(Nyyti ry)</a> <br />
-       Kokeile vapaan kirjoittamisen tekniikoita: <a href='https://blogs.helsinki.fi/kielijelppi/ideointivaihe/' target="_blank" rel="noopener noreferrer">(Kielijelppi)</a> <br />
-       Jos sinua mietityttää esim. lukivaikeuteen tai mielen hyvinvointiin liittyvät asiat, saat lisätietoa korkeakoulusi verkkosivuilta <a href='https://studies.helsinki.fi/ohjeet/artikkeli/opiskelun-tueksi' target="_blank" rel="noopener noreferrer">(HY)</a> <a href='https://www.utu.fi/fi/opiskelijaksi/opiskelijan%20palvelut%20ja%20hyvinvointi' target="_blank" rel="noopener noreferrer">(TY)</a> <a href='https://www.tuni.fi/fi/opiskelijan-opas/tampereen-yliopiston-opiskelijan-opas/opiskelijoiden-hyvinvointi' target="_blank" rel="noopener noreferrer">(TUNI)</a> <a href='https://www.jyu.fi/fi/opiskelijalle/hyvinvointi/apua-ja-tukea-opiskeluhyvinvointiin'target="_blank" rel="noopener noreferrer">(JYU)</a> <a href='https://kamu.uef.fi/tietopankki/uudelle-opiskelijalle/palveluita-opintojen-alussa/' target="_blank" rel="noopener noreferrer">(UEF)</a> <a href='https://www.uwasa.fi/fi/opiskelijat' target="_blank" rel="noopener noreferrer">(VY)</a> <a href='https://www.oulu.fi/fi/opiskelijalle/tukea-opiskeluun' target="_blank" rel="noopener noreferrer">(OY)</a> <a href='https://www.ulapland.fi/FI/Opiskelu/Ohjaus-ja-hyvinvointi' target="_blank" rel="noopener noreferrer">(LY)</a> <br />
-       Millaisia tekstilajeja opiskelija tarvitsee? <a href='https://blogs.helsinki.fi/kielijelppi/opiskelijoiden-tekstilajeja/' target="_blank" rel="noopener noreferrer">(Kielijelppi)</a> <br />
-       Akateeminen essee for dummies: <a href='http://www.plotti.fi/2021/02/23/akateeminen-opiskeluessee-for-dummies/' target="_blank" rel="noopener noreferrer">(Plotti)</a> <br />
-       Lue akateemisista käytänteistä e-kirjasta <a href='https://www.ellibslibrary.com/book/9789523455115' target="_blank" rel="noopener noreferrer">Tiede ja teksti - Tehoa ja taitoa tutkielman kirjoittamiseen</a> (lainattavissa Haka-tunnuksilla ja kirjastoista) <br />
-       Lukeminen ja referoiminen: <a href='https://blogs.helsinki.fi/kielijelppi/lukeminen-ja-referoiminen/' target="_blank" rel="noopener noreferrer">(Kielijelppi)</a> <br />
-       Tiedon käyttö ja arviointi: <a href='https://blogs.helsinki.fi/opiskelijan-digitaidot/3-tiedonhankinta/3-4-loydetyn-tiedon-kaytto-ja-arviointi/' target="_blank" rel="noopener noreferrer">(Opiskelijan digitaidot) </a> <br />
-        <a href='https://tiedelukutaito.mooc.fi/part-5/2-lahteiden-kaytto-ja-tekijanoikeudet' target="_blank" rel="noopener noreferrer">Lähteiden käyttö ja tekijänoikeudet</a>  <br />
-       Lue argumentoinnista, referoimisesta ja viittaamisesta (luvut 5 ja 6) e-kirjasta <a href='https://www.ellibslibrary.com/book/9789523455115' target="_blank" rel="noopener noreferrer">Tiede ja teksti - Tehoa ja taitoa tutkielman kirjoittamiseen</a> (lainattavissa Haka-tunnuksilla ja kirjastoista) <br />
-      </p>
-    </div>
-  )
-}
+import { Box, Link, List, ListItem, Typography } from '@mui/material'
+
+const Links = () => (
+  <Box sx={{ p: 4 }} >
+    <Typography variant='h3'>Hyödyllisiä linkkejä</Typography>
+    <List>
+      <ListItem>
+        <Typography sx={{ pr: 0.5 }} display='inline'>
+            Lue lisää yhdessä kirjoittamisesta:
+          <Link href="https://webcgi.oulu.fi/oykk/abc/kirjoittamisprosessi/yhdessa_kirjoittaminen_ja_yhteiskirjoittaminen/">(Kirjoittajan ABC)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography sx={{ pr: 0.5 }} display='inline'>
+            Lue lisää motivaatiosta ja tavoitteiden asettamisesta:
+          <Link href='https://gradutakuu.fi/sisallys/'>(Gradutakuu)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography sx={{ pr: 0.5 }} display='inline'>
+            Lue lisää akateemisista tekstitaidoista:
+          <Link href ='https://kokonaisvaltainenkirjoittaminen.fi/2011/02/15/akateemisen-kirjoittamisen-opetus/'>(Kokonaisvaltainen kirjoittaminen -blogi)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography sx={{ pr: 0.5 }} display='inline'>
+            Lue tieteellisen referoimisesta (luku 6) e-kirjasta Tiede ja teksti - Tehoa ja taitoa tutkielman kirjoittamiseen
+            (lainattavissa Haka-tunnuksilla ja kirjastoista):
+          <Link href='https://www.ellibslibrary.com/book/9789523455115'>https://www.ellibslibrary.com/book/9789523455115</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography sx={{ pr: 0.5 }} display='inline'>
+            Lue lisää palautteesta:
+          <Link href = 'http://www.plotti.fi/2020/08/14/palautetta-hopeatarjottimella-vaikuta-palautekokemuksiisi/'>(Plotti)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Lue kirjoittamisen sosiaalisesta luonteesta:
+          <Link href='https://antroblogi.fi/2017/06/sinun-tekstisi-ei-ole-sinun/'>(Sinun tekstisi ei ole sinun)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Lue tieteellisen tekstin rakenteesta ja argumentoinnista (luvut 4 ja 5) e-kirjasta Tiede ja teksti
+            - Tehoa ja taitoa tutkielman kirjoittamiseen (lainattavissa Haka-tunnuksilla ja kirjastoista):
+          <Link href='https://www.ellibslibrary.com/book/9789523455115'>https://www.ellibslibrary.com/book/9789523455115</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Tukea tekstin viimeistelyyn:
+          <Link href='https://blogs.helsinki.fi/kielijelppi/viimeistely/'>(Kielijelppi)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Kangertaako kandityö?:
+          <Link href='http://www.plotti.fi/2018/01/29/kangertaako-kandityo/'>(Plotti)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Miten vähentää prokrastinaatiota?:
+          <Link href='https://www.nyyti.fi/opiskelijoille/opi-elamantaitoa/ajanhallinta/prokrastinaatio/miten-vahentaa-prokrastinaatiota/'>(Nyyti ry)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Kokeile vapaan kirjoittamisen tekniikoita:
+          <Link href='https://blogs.helsinki.fi/kielijelppi/ideointivaihe/'>(Kielijelppi)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+            Jos sinua mietityttää esim. lukivaikeuteen tai mielen hyvinvointiin liittyvät asiat, saat lisätietoa korkeakoulusi verkkosivuilta
+          <Link href='https://studies.helsinki.fi/ohjeet/artikkeli/opiskelun-tueksi'>(HY)</Link>
+        </Typography>
+      </ListItem>
+      <ListItem>
+        <Typography>
+          <Link href='http://www.kielitoimistonohjepankki.fi/'>Kielitoimiston ohjepankki</Link>
+        </Typography>
+      </ListItem>
+    </List>
+  </Box>
+)
 
 export default Links
