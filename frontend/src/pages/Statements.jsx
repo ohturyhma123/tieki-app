b7d3f04f44a04306eb0a2c5b651bde1120ed603d
w <weverhall@gmail.com>
2023-10-15 23:51:52 +0300

    resolve

--- frontend/src/pages/Statements.jsx
+++ frontend/src/pages/Statements.jsx
@@ -67,7 +67,7 @@ const Statements = () => {
         [currentStatementSetIndex]: prevState[currentStatementSetIndex] - 1,
       }))
     } else {
-      if (selectedStatementsCount < 3) {
+      if (selectedStatementsCountOnPage[currentStatementSetIndex] < 3) {
         // Statement is not selected, so select it and increase the count
         setSelectedStatements((prevStatements) => [...prevStatements, statementId])
         setSelectedStatementsCount((prevCount) => {
@@ -111,14 +111,42 @@ const Statements = () => {
     Handles going back to the previous statement set.
   */
   const handlePreviousStatementSet = () => {
-    if (visitedStatementSetIndices.length > 0) {
-      const previousIndex = visitedStatementSetIndices.pop()
+    if (urlIndex > 0) {
+      const previousIndex = urlIndex - 1
       setCurrentStatementSetIndex(previousIndex)
       const previousCount = selectedStatementsCountHistory.pop()
       setSelectedStatementsCount(previousCount)
       navigate(-1)
     }
   }
+
+  /**
+    Loads selectedStatements and selectedStatementsCount from sessionStorage on component mount
+    when reloading the page.
+  */
+  useEffect(() => {
+    const savedStatements = sessionStorage.getItem('selectedStatements')
+    const savedCounts = sessionStorage.getItem('selectedStatementsCountOnPage')
+
+    if (savedStatements) {
+      setSelectedStatements(JSON.parse(savedStatements))
+    }
+
+    if (savedCounts) {
+      setSelectedStatementsCountOnPage(JSON.parse(savedCounts))
+    }
+  }, [])
+
+  /**
+    Save selectedStatements and selectedStatementsCount to sessionStorage whenever they change
+  */
+  useEffect(() => {
+    sessionStorage.setItem('selectedStatements', JSON.stringify(selectedStatements))
+  }, [selectedStatements])
+
+  useEffect(() => {
+    sessionStorage.setItem('selectedStatementsCountOnPage', JSON.stringify(selectedStatementsCountOnPage))
+  }, [selectedStatementsCountOnPage])
 
   let statements
   let setIndex
@@ -158,9 +186,9 @@ const Statements = () => {
         <Typography variant='h5'>Väittämäsetti {currentStatementSetIndex + 1}/{statementsData.length}</Typography>
         <Typography sx={{ py: 2, fontStyle: 'italic' }}>Voit valita enintään kolme vaihtoehtoa.</Typography>
         {/**
-          Iterate through the array and create an element for each statement.
-          Conditionally add the "selected" CSS class if the statement is in the "selectedStatements" array.
-        */}
+        Iterate through the array and create an element for each statement.
+        Conditionally add the "selected" CSS class if the statement is in the "selectedStatements" array.
+      */}
         {statements.map((s) => (
           <div
             key={s.id}
@@ -173,7 +201,7 @@ const Statements = () => {
         ))}
         {/** Using ternary conditional operators, show different button text when there are no statement sets left */}
         <p>
-          {visitedStatementSetIndices.length > 0 && (
+          {urlIndex > 0 && (
             <Button id='previous-btn' sx={{ mr: 1 }} variant="contained" onClick={handlePreviousStatementSet}>Edellinen</Button>
           )}
           {currentStatementSetIndex < statementsData.length - 1
