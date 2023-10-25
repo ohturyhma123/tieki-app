da69d19fc7bda2d978b7471d424ca24d246dea44
katajak <101673344+katajak@users.noreply.github.com>
2023-10-25 15:16:46 +0300

    backend stuff

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 const app = express()
 import cors from 'cors'
 import linksRouter from './controllers/links.js'
@@ -9,5 +10,11 @@ app.use(express.json())
 
 app.use('/api/links', linksRouter)
 app.use('/api/statements', statementsRouter)
+
+const DIST_PATH = path.resolve('dist')
+const INDEX_PATH = path.resolve(DIST_PATH, 'index.html')
+
+app.use(express.static(DIST_PATH))
+app.get('*', (_, res) => res.sendFile(INDEX_PATH))
 
 export default app
