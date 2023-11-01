2f4cff47ae5d58d32747ba40ca9547d31fb37777
Nikita Essine <nikita.essine@helsinki.fi>
2023-11-01 15:28:25 +0200

    ui changes for editlinks

--- frontend/components/EditLinks.jsx
+++ frontend/components/EditLinks.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import { TextField, Button } from '@mui/material'
+import { TextField, Button, Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import '../assets/EditLinks.css'
+import homeBackground from '../assets/home-background.jpg'
+import monochromeBackground from '../assets/monochrome-background.jpg'
 import AdminCheck from '../functions/AdminCheck'
 
 const baseUrl = '/api/links'
@@ -26,6 +29,17 @@ const EditLinks = () => {
     }
 
     fetchData()
+  }, [])
+
+  /**
+   * Prevent scrolling when the component is mounted.
+   */
+  
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
   }, [])
 
   /**
@@ -84,37 +98,55 @@ const EditLinks = () => {
 
   return (
     <div className="editLinksContainer">
-      <div>
-        <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>
-        Muokkaa linkkejä
-        </h1>
+      <div className="centeredContent" style={{ overflowY: 'auto', maxHeight: '80vh' }}>
+        <picture>
+          <source media="(max-width: 1200px), (max-height: 700px)" srcSet={monochromeBackground} />
+          <img
+            src={homeBackground}
+            alt="homeBackground"
+            style={{ maxWidth: '100%', maxHeight: '100%', position: 'absolute', top: 0, left: 0, right: 0,
+              width: '100%', height: '100%', zIndex: -1 }}
+          />
+        </picture>
+        <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px', fontFamily: '"Lato", sans-serif' }}>Muokkaa linkkejä</h1>
         {links.map((link) => (
-          <div key={link.id} style={{ marginBottom: '20px', display: 'flex' }}>
-            <TextField
-              id={`name-${link.id}`}
-              label="Linkin nimi"
-              variant="outlined"
-              value={link.name}
-              onChange={(e) => handleNameChange(link.id, e.target.value)}
-              style={{ marginRight: '20px', resize: 'both' }}
-            />
-            <TextField
-              id={`description-${link.id}`}
-              label="Kuvaus"
-              variant="outlined"
-              value={link.description}
-              onChange={(e) => handleDescriptionChange(link.id, e.target.value)}
-              style={{ marginRight: '20px' }}
-            />
-            <TextField
-              id={`url-${link.id}`}
-              label="Linkin URL"
-              variant="outlined"
-              value={link.url}
-              onChange={(e) => handleUrlChange(link.id, e.target.value)}
-              style={{ marginRight: '20px' }}
-            />
-          </div>
+          <Accordion key={link.id} style={{
+            marginBottom: '20px',
+            backgroundColor: '#f8f8f8',
+            border: '1px solid #ddd',
+            borderRadius: '8px',
+            boxShadow: 'none',
+          }}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} >
+              <Typography>{link.name}</Typography>
+            </AccordionSummary>
+            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
+              <TextField
+                id={`name-${link.id}`}
+                label="Linkin nimi"
+                variant="outlined"
+                value={link.name}
+                onChange={(e) => handleNameChange(link.id, e.target.value)}
+                style={{ marginBottom: '20px' }}
+              />
+              <TextField
+                id={`description-${link.id}`}
+                label="Kuvaus"
+                variant="outlined"
+                value={link.description}
+                onChange={(e) => handleDescriptionChange(link.id, e.target.value)}
+                style={{ marginBottom: '20px' }}
+              />
+              <TextField
+                id={`url-${link.id}`}
+                label="Linkin URL"
+                variant="outlined"
+                value={link.url}
+                onChange={(e) => handleUrlChange(link.id, e.target.value)}
+                style={{ marginBottom: '20px' }}
+              />
+            </AccordionDetails>
+          </Accordion>
         ))}
         <Button
           variant="contained"
@@ -122,7 +154,7 @@ const EditLinks = () => {
           onClick={handleSaveClick}
           style={{ backgroundColor: '#00000', marginBottom: '20px' }}
         >
-        Tallenna
+          Tallenna
         </Button>
       </div>
     </div>
