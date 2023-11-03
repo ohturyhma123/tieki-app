d3067a858152aeac82c04126e80ff703fee5247a
katajak <101673344+katajak@users.noreply.github.com>
2023-11-03 15:47:33 +0200

    fix

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -13,13 +13,8 @@ loginRouter.get('/login', async (req, res) => {
 
 loginRouter.get('/oidc', (req, res, next) => {
   if (req.user) {
-    // If the user is already logged in and an admin, redirect to '/edit'
-    // If the user is already logged in but not an admin, send 403 Forbidden
-    if (req.user.isAdmin) {
-      res.redirect('/edit')
-    } else {
-      res.status(403).send('Forbidden')
-    }
+    // If the user is already logged in, redirect to '/edit'
+    res.redirect('/edit')
   } else {
     // If the user is not logged in, proceed with the OIDC authentication
     passport.authenticate('oidc')(req, res, next)
