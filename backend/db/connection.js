364378848d78f2deaadd9d19e42af391bdd12254
Janika Kalliokoski <janika.kalliokoski@gmail.com>
2023-11-07 21:48:38 +0200

    try api test run

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
-import { MONGODB_TEST_URI } from '../util/config.js'
+import { MONGODB_TEST_URI, MONGODB_URI } from '../util/config.js'
 
 const connectToDatabase = () => {
-  mongoose.connect(MONGODB_TEST_URI)
+  const uri = process.env.NODE_ENV === 'test' ? MONGODB_TEST_URI : MONGODB_URI
+
+  mongoose.connect(uri)
   const connection = mongoose.connection
 
   connection.on('error', console.error.bind(console, 'MongoDB connection error:'))
