18fb2fd6badeae0e0d3a90e0cb68c204eb26f26d
katajak <101673344+katajak@users.noreply.github.com>
2023-11-15 22:49:21 +0200

    fix mongostore db connection

--- backend/util/mongostore.js
+++ backend/util/mongostore.js
@@ -1,11 +1,11 @@
 import session from 'express-session'
 import MongoDBStore from 'connect-mongodb-session'
-import { MONGODB_URI } from './config.js'
+import { MONGODB_TEST_URI, MONGODB_URI, inTestMode } from './config.js'
 
 const MongoDBStoreSession = MongoDBStore(session)
 
 const store = new MongoDBStoreSession({
-  uri: MONGODB_URI,
+  uri: inTestMode ? MONGODB_TEST_URI : MONGODB_URI,
   collection: 'sessions',
 })
 
