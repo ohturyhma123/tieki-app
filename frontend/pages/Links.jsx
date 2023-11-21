6f33fca0a73189d3d458e134ad228295fa241c8d
w <weverhall@gmail.com>
2023-11-21 15:56:45 +0200

    add conditional formatting so that even lengthy link text gets appropriate line breaks

--- frontend/pages/Links.jsx
+++ frontend/pages/Links.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Box, Link, List, ListItem, Typography, Grid, CircularProgress, Container, Paper } from '@mui/material'
 import useApi from '../hooks/useApi'
 import monochromeBackground from '../assets/monochrome-background.jpg'
@@ -29,7 +30,23 @@ const Links = () => {
   const formatLinkText = (text) => {
     // Check if the text ends with punctuation other than ? and remove it
     if (text.endsWith('.' || ',' || ':' || ';')) {
-      return text.slice(0, -1)
+      text = text.slice(0, -1)
+    }
+
+    // Insert line break every 275 characters
+    const chunks = text.match(/.{1,275}(\s|$)/g)
+    if (chunks) {
+      return chunks.map((chunk, index, array) => (
+        <React.Fragment key={index}>
+          {index < array.length - 1 ? (
+            <>
+              {chunk}<br />
+            </>
+          ) : (
+            chunk
+          )}
+        </React.Fragment>
+      ))
     }
     return text
   }
@@ -48,12 +65,35 @@ const Links = () => {
           <List>
             {links.map((link, index) => (
               <ListItem key={index} id={`${index + 1}`}>
-                <Typography sx={{ fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
-                  {formatLinkText(link.description)}:
-                  <Link sx={{ pl: 0.6 }} href={link.url} target="_blank" rel="noopener noreferrer">
-                    {link.name}
-                  </Link>
-                </Typography>
+                {/** If either description or name length is over 180 chars,
+                 reduce font size and insert appropriate line breaks via formatLinkText function
+                */}
+                {(link.description.length > 180 || link.name.length > 180) ? (
+                  <Typography sx={{ fontSize: 12, fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                    {formatLinkText(link.description)}:
+                    <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
+                      <br />{formatLinkText(link.name)}
+                    </Link>
+                  </Typography>
+                ) : (
+                  <Typography sx={{ fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                    {formatLinkText(link.description)}:
+                    {/** If description and name length sum is 175 chars or less, don't insert a line break */}
+                    {((link.description + link.name).length) <= 175 ? (
+                      <>
+                        <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
+                          {link.name}
+                        </Link>
+                      </>
+                    ) : (
+                      <>
+                        <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
+                          <br />{link.name}
+                        </Link>
+                      </>
+                    )}
+                  </Typography>
+                )}
               </ListItem>
             ))}
           </List>
