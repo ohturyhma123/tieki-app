9eb014ddf6d03e5a34881c40f53f412aa88e49f9
w <weverhall@gmail.com>
2023-11-21 14:15:55 +0200

    add background, paper, font, etc. for linkspage

--- frontend/pages/Links.jsx
+++ frontend/pages/Links.jsx
@@ -1,5 +1,6 @@
-import { Box, Link, List, ListItem, Typography, Grid, CircularProgress } from '@mui/material'
+import { Box, Link, List, ListItem, Typography, Grid, CircularProgress, Container, Paper } from '@mui/material'
 import useApi from '../hooks/useApi'
+import monochromeBackground from '../assets/monochrome-background.jpg'
 
 const Links = () => {
   const { data: links, loading, error } = useApi('/api/links')
@@ -34,22 +35,30 @@ const Links = () => {
   }
 
   return (
-    <Grid sx={{ background: '#FEF6E1' }} container direction="column" justifyContent="center" alignItems="center">
-      <Box sx={{ p: 5 }}>
-        <Typography sx={{ pb: 3 }} variant='h3'>Hyödyllisiä linkkejä</Typography>
-        <List>
-          {links.map((link, index) => (
-            <ListItem key={index} id={`${index + 1}`}>
-              <Typography>
-                {formatLinkText(link.description)}:
-                <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
-                  ({link.name})
-                </Link>
-              </Typography>
-            </ListItem>
-          ))}
-        </List>
-      </Box>
+    <Grid container direction="column" justifyContent="center" alignItems="center">
+      <img
+        src={monochromeBackground}
+        alt="monochrome background"
+        style={{ maxWidth: '100%', position: 'fixed', top: 0, left: 0, right: 0,
+          width: '100%', height: '100%', zIndex: -1 }}
+      />
+      <Container maxWidth='xl'>
+        <Paper sx={{ m: -1, mt: 4, mb: 4, p: 3, pb: 5, background: '#FCF8F4' }} variant='elevation' elevation={10}>
+          <Typography sx={{ textAlign: 'center', fontFamily: '"Lato", sans-serif', color: '#00011b', mb: 5, mt: 0.5 }} variant='h3'>Hyödyllisiä linkkejä</Typography>
+          <List>
+            {links.map((link, index) => (
+              <ListItem key={index} id={`${index + 1}`}>
+                <Typography sx={{ fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                  {formatLinkText(link.description)}:
+                  <Link sx={{ pl: 0.6 }} href={link.url} target="_blank" rel="noopener noreferrer">
+                    {link.name}
+                  </Link>
+                </Typography>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Container>
     </Grid>
   )
 }
