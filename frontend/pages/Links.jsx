1549b68caaf711da0f8bedba5490b20ca19a1b62
w <weverhall@gmail.com>
2023-11-21 17:03:04 +0200

    add conditional font, paper and line break handling for mobile ui

--- frontend/pages/Links.jsx
+++ frontend/pages/Links.jsx
@@ -5,6 +5,7 @@ import monochromeBackground from '../assets/monochrome-background.jpg'
 
 const Links = () => {
   const { data: links, loading, error } = useApi('/api/links')
+  const isMobile = window.innerWidth <= 480
 
   if (loading) {
     return (
@@ -28,12 +29,12 @@ const Links = () => {
   }
 
   const formatLinkText = (text) => {
-    // Check if the text ends with punctuation other than ? and remove it
+    /** Check if the text ends with punctuation other than ? and remove it. */
     if (text.endsWith('.' || ',' || ':' || ';')) {
       text = text.slice(0, -1)
     }
 
-    // Insert line break every 275 characters
+    /** Insert line break every 275 characters. */
     const chunks = text.match(/.{1,275}(\s|$)/g)
     if (chunks) {
       return chunks.map((chunk, index, array) => (
@@ -60,39 +61,52 @@ const Links = () => {
           width: '100%', height: '100%', zIndex: -1 }}
       />
       <Container maxWidth='xl'>
-        <Paper sx={{ m: -1, mt: 4, mb: 4, p: 3, pb: 5, background: '#FCF8F4' }} variant='elevation' elevation={10}>
-          <Typography sx={{ textAlign: 'center', fontFamily: '"Lato", sans-serif', color: '#00011b', mb: 5, mt: 0.5 }} variant='h3'>Hyödyllisiä linkkejä</Typography>
+        <Paper sx={{ m: isMobile ? 0 : -1, mt: 4, mb: 4, p: isMobile ? 2 : 3, pb: 5, background: '#FCF8F4' }}
+          variant='elevation' elevation={10}>
+          <Typography sx={{ textAlign: 'center', fontFamily: '"Lato", sans-serif', color: '#00011b', mb: isMobile ? 3 : 5, mt: 0.5 }} variant='h3'>Hyödyllisiä linkkejä</Typography>
           <List>
             {links.map((link, index) => (
               <ListItem key={index} id={`${index + 1}`}>
-                {/** If either description or name length is over 180 chars,
-                 reduce font size and insert appropriate line breaks via formatLinkText function
-                */}
-                {(link.description.length > 180 || link.name.length > 180) ? (
-                  <Typography sx={{ fontSize: 12, fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                {/** Handle line breaks for mobile UI. */}
+                {isMobile ? (
+                  <Typography sx={{ fontSize: 14, fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
                     {formatLinkText(link.description)}:
                     <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
-                      <br />{formatLinkText(link.name)}
+                      {(link.description + link.name).length <= 60 && (link.description + link.name).length >= 35 ? (
+                        <><br />{link.name}</>
+                      ) : (
+                        <>{link.name}</>
+                      )}
                     </Link>
                   </Typography>
+                  /** End of the code for mobile UI, start of desktop UI code. */
                 ) : (
-                  <Typography sx={{ fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
-                    {formatLinkText(link.description)}:
-                    {/** If description and name length sum is 175 chars or less, don't insert a line break */}
-                    {((link.description + link.name).length) <= 175 ? (
-                      <>
+                  /**
+                    If either description or name length is over 180 chars,
+                    reduce font size and insert appropriate line breaks via formatLinkText function.
+                  */
+                  (link.description.length > 180 || link.name.length > 180) ? (
+                    <Typography sx={{ fontSize: 12, fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                      {formatLinkText(link.description)}:
+                      <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
+                        <br />{formatLinkText(link.name)}
+                      </Link>
+                    </Typography>
+                  ) : (
+                    /** If description and name length sum is 175 chars or less, don't insert a line break. */
+                    <Typography sx={{ fontFamily: '"Lato", sans-serif', color: '#00011b' }}>
+                      {formatLinkText(link.description)}:
+                      {((link.description + link.name).length) <= 175 ? (
                         <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
                           {link.name}
                         </Link>
-                      </>
-                    ) : (
-                      <>
+                      ) : (
                         <Link sx={{ pl: 0.5 }} href={link.url} target="_blank" rel="noopener noreferrer">
                           <br />{link.name}
                         </Link>
-                      </>
-                    )}
-                  </Typography>
+                      )}
+                    </Typography>
+                  )
                 )}
               </ListItem>
             ))}
