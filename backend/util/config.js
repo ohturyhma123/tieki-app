e33dbd0260d1d2e1435716be99124e19aac294dc
katajak <101673344+katajak@users.noreply.github.com>
2023-11-25 02:14:05 +0200

    use workflow specific mongodb for actions

--- backend/util/config.js
+++ backend/util/config.js
@@ -10,9 +10,7 @@ export const inProduction = !inStaging && process.env.NODE_ENV === 'production'
 
 export const inTestMode = process.env.NODE_ENV === 'test'
 
-export const MONGODB_URI = process.env.MONGODB_URI || ''
-
-export const MONGODB_TEST_URI = process.env.MONGODB_TEST_URI || ''
+export const MONGODB_URI = inTestMode ? 'mongodb://localhost:27017/tieki-app' : process.env.MONGODB_URI || ''
 
 export const OIDC_ISSUER = inProduction
   ? 'https://login.helsinki.fi/.well-known/openid-configuration'
@@ -22,10 +20,10 @@ export const OIDC_LOGOUT_URI = inProduction
   ? 'https://login.helsinki.fi/idp/profile/Logout'
   : 'https://login-test.it.helsinki.fi/idp/profile/Logout'
 
-export const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || ''
+export const OIDC_CLIENT_ID = process.env.OIDC_CLIENT_ID || '<client_id>'
 
-export const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET || ''
+export const OIDC_CLIENT_SECRET = process.env.OIDC_CLIENT_SECRET || '<client_secret>'
 
-export const OIDC_REDIRECT_URI = process.env.OIDC_REDIRECT_URI || ''
+export const OIDC_REDIRECT_URI = process.env.OIDC_REDIRECT_URI || '<redirect_uri>'
 
-export const SESSION_SECRET = process.env.SESSION_SECRET || ''
+export const SESSION_SECRET = process.env.SESSION_SECRET || '<session_secret>'
