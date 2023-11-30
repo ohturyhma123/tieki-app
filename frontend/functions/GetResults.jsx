340707a4b387d9d2ae5abb0dd7a33341a5407a49
Juska <juseljus@gmail.com>
2023-11-30 02:51:50 +0200

    sort results

--- frontend/functions/GetResults.jsx
+++ frontend/functions/GetResults.jsx
@@ -1,10 +1,17 @@
 const GetResults = (scores, resultsData) => {
-
+  
   const positiveCategories = Object.keys(scores).filter((category) => scores[category][0] >= 2)
   const negativeCategories = Object.keys(scores).filter((category) => scores[category][1] <= -2)
 
+  resultsData.forEach((result) => {
+    result.scores = scores[result.category]
+  })
+
   const positiveResults = resultsData.filter((result) => positiveCategories.includes(result.category) && result.positive)
   const negativeResults = resultsData.filter((result) => negativeCategories.includes(result.category) && result.positive === false)
+
+  positiveResults.sort((a,b) => b.scores[0] - a.scores[0])
+  negativeResults.sort((a,b) => a.scores[1] - b.scores[1])
 
   return [positiveResults, negativeResults]
 }
