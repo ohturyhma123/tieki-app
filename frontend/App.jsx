44310db2831df1524f6305fb021b9785be62add5
alanenpa <72764484+alanenpa@users.noreply.github.com>
2023-12-05 16:48:48 +0200

    Create about page

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Statements from './pages/Statements'
 import Results from './pages/Results'
 import Links from './pages/Links'
+import About from './pages/About'
 import Edit from './pages/Edit'
 import EditLinks from './components/EditLinks'
 import Login from './pages/Login'
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/results" element={<Results />} />
         <Route path="/pdfview" element={<PDFView />} />
         <Route path="/links" element={<Links />} />
+        <Route path="/about" element={<About />} />
         <Route path="/edit" element={<Edit />} />
         <Route path="/edit/links" element={<EditLinks />} />
         <Route path="/edit/statements" element={<EditStatements />} />
---
+++ frontend/pages/About.jsx
@@ -0,0 +1,42 @@
+import { Box, Container, Grid, List, ListItem, Paper, Typography } from '@mui/material'
+import monochromeBackground from '../assets/monochrome-background.jpg'
+
+
+const About = () => {
+  return (
+    <Grid container direction="column" justifyContent="center" alignItems="center">
+      <img
+        src={monochromeBackground}
+        alt="monochrome background"
+        style={{ maxWidth: '100%', position: 'fixed', top: 0, left: 0, right: 0,
+          width: '100%', height: '100%', zIndex: -1 }}
+      />
+      <Grid item display='flex' justifyContent="center" alignItems="center">
+        <Container maxWidth='xl' display='flex' justifyContent="center" alignItems="center">
+          <Paper sx={{ m: -1, mt: 3, mb: 5, mx: 'auto', p: 2, pb: 7, background: '#FCF8F4', width: '80%' }} variant='elevation' elevation={10} display='flex' justifyContent="center" alignItems="center">
+            <Box display='flex' justifyContent="center" alignItems="center">
+              <Typography sx={{ pt: 3, pl: 2 }} variant='h6'>
+                Tieteellisen kirjoittamisen itsearviointitestin sisällön on tuottanut Helsingin yliopiston kielikeskuksen äidinkielen ja finskan yksikkö.
+                Testi julkaistiin alun perin vuonna 2013. Testin uusitun version laativat seuraavat Helsingin yliopiston tietojenkäsittelytieteen kandiohjelman
+                ohjelmistotuotantoprojekti-kurssin opiskelijat syksyllä 2023:
+                <Typography variant='subtitle1'>
+                  <List dense sx={{ listStyleType: 'disc', pl: 4 }}>
+                    <ListItem sx={{ display: 'list-item' }}>Ilkka Luukkonen</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Janika Kalliokoski</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Juska Juselius</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Kristian Kataja</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Nikita Essine</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Patrik Alanen</ListItem>
+                    <ListItem sx={{ display: 'list-item' }}>Wäinö Everhall</ListItem>
+                  </List>
+                </Typography>
+              </Typography>
+            </Box>
+          </Paper>
+        </Container>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default About
