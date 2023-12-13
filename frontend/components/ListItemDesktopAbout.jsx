782545ebfba464ef5afb6b6257b0b751ea8f63aa
w <weverhall@gmail.com>
2023-12-13 12:14:21 +0200

    improve layout and font, add icons, open links in new tab

---
+++ frontend/components/ListItemDesktopAbout.jsx
@@ -0,0 +1,25 @@
+import { Typography, ListItemText, Link } from '@mui/material'
+import SchoolIcon from '@mui/icons-material/School'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+
+const ListItemDesktop = ({ text, link }) => {
+  return (
+    <ListItem sx={{ margin: '-4px 0' }}>
+      <ListItemIcon sx={{ color: '#00011b' }}>
+        <SchoolIcon style={{ fontSize: '17px' }} />
+      </ListItemIcon>
+      <ListItemText
+        primary={
+          <Typography
+            variant='body2'
+            style={{ color: '#00011b', fontSize: '17px', fontFamily: '"Lato", sans-serif', fontWeight: '500', marginLeft: -23 }}>
+            {text}{' '}
+            (<Link href={link} target="_blank" rel="noopener noreferrer">GitHub</Link>)
+          </Typography>
+        }/>
+    </ListItem>
+  )
+}
+
+export default ListItemDesktop
